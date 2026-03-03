6f1ffd4c2ec4b252504082ab0c31c97cf87af76f
Hyun cao <hyun@arrowhitech.com>
2026-03-03 15:34:58 +0700

    UI: Move logout button inside nav so it scrolls and doesn't overlap on zoom

--- modules/includes/sidebar.php
+++ modules/includes/sidebar.php
@@ -130,22 +130,22 @@ function isMenuItemActive($path, $current_uri)
                 <span>Settings</span>
             </a>
         <?php endif; ?>
-    </nav>
 
-    <div class="sidebar-footer">
-        <a href="/logout" class="nav-item logout">
-            <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path
-                    d="M9 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H9"
-                    stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
-                <path d="M16 17L21 12L16 7" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round" />
-                <path d="M21 12H9" stroke="currentColor" stroke-width="2" stroke-linecap="round"
-                    stroke-linejoin="round" />
-            </svg>
-            <span>Logout</span>
-        </a>
-    </div>
+        <div class="sidebar-footer" style="margin-top: auto; padding-top: 1rem; border-top: 1px solid #334155;">
+            <a href="/logout" class="nav-item logout">
+                <svg viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path
+                        d="M9 21H5C4.46957 21 3.96086 20.7893 3.58579 20.4142C3.21071 20.0391 3 19.5304 3 19V5C3 4.46957 3.21071 3.96086 3.58579 3.58579C3.96086 3.21071 4.46957 3 5 3H9"
+                        stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+                    <path d="M16 17L21 12L16 7" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                    <path d="M21 12H9" stroke="currentColor" stroke-width="2" stroke-linecap="round"
+                        stroke-linejoin="round" />
+                </svg>
+                <span>Logout</span>
+            </a>
+        </div>
+    </nav>
 </aside>
 
 <script>
