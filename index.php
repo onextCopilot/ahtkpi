7863305ee66eb10e75f8631fead6be67d1a79d53
HyunOnext <130048111+HyunOnext@users.noreply.github.com>
2026-05-26 15:56:12 +0700

    Update index.php

--- index.php
+++ index.php
@@ -92,6 +92,7 @@ $routes = [
     '/api/kpi_monthly_save' => 'api/kpi_monthly_save.php',
     '/api/kpi_quarterly_save' => 'api/kpi_quarterly_save.php',
     '/api/mark_notification_read' => 'api/mark_notification_read.php',
+    '/api/notifications/mark_read' => 'api/notifications_mark_read.php',
     '/sale-orders' => 'modules/sale_orders/index.php',
     '/api/sale_orders' => 'api/sale_orders.php',
     '/my-reports' => 'modules/sale_reports/index.php',
